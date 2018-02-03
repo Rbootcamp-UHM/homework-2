835ef51c91b9deea8a4e495ff5c7c1cab50bfefa
mbutler808 <35955837+mbutler808@users.noreply.github.com>
2018-02-02 15:24:08 -1000

    Edit HW2_stub.R and change to stub to your initials
    Or write your own

---
+++ HW2_stub.R
@@ -0,0 +1,23 @@
+##########   Homework 2   Due Monday by 5pm
+##
+## Follow the instructions in HW2.pdf
+##
+## You may use the following stub to get started and fill in the rest. 
+## Name your script HW2_myinitials.R
+## make sure it runs from source without error or warnings - Test it after shutting down R
+## Write clean code without unnecessary clutter.
+## Submit it on Github or by email with subject line "710 Homework 2"
+
+## Question 1
+# 1a. 
+
+	x <- 
+
+# 1b. 
+#	include some code to show how you figured out the answer
+
+# The attributes of x are ...
+
+## Complete questions 1 and 2, answering questions in comments
+## Test your script by running it from source("HW1_myinitials.R")  from the command line before turning in.
+## You may delete whatever instructions/comments you like - do include comments to make your script understandable to someone outside of class, an use your judgement to make your script readable please. 
